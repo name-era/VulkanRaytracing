cd2016d35fe956ebd0d4feaa86cd7f8db1620350
name-era <mkuwana@keio.jp>
2022-02-04 21:04:55 +0900

    UI用のシェーダーを追加

--- Shaders/compoiel.bat
+++ Shaders/compoiel.bat
@@ -1,3 +1,5 @@
 C:\VulkanSDK\1.2.198.1\Bin/glslc.exe mesh.vert -o mesh.vert.spv
 C:\VulkanSDK\1.2.198.1\Bin/glslc.exe mesh.frag -o mesh.frag.spv
+C:\VulkanSDK\1.2.198.1\Bin/glslc.exe ui.frag -o ui.frag.spv
+C:\VulkanSDK\1.2.198.1\Bin/glslc.exe ui.frag -o ui.frag.spv
 pause
